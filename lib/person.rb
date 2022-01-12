654c3f161907ad58b3bfa8884fdb89348a28710f
Jason Botesazan <jbotesazan@gmail.com>
2022-01-12 11:29:59 -0500

    commit tree f1625ed0d6f6a894a48951192e3e462039d2cea2

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+class Person
+    attr_accessor = :bank_account
+    attr_reader = :hygiene, :happiness, :name
+    attr_writer =
+
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+    end
+end
